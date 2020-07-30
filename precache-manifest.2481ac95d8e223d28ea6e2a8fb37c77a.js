9e77ccb6e2a3b79a3455b5cdcd852470cedcfceb
CeeDeeBee <cdbarnes98@gmail.com>
2020-07-30 19:35:16 -0400

    Updates

---
+++ precache-manifest.2481ac95d8e223d28ea6e2a8fb37c77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbdee886e3db2216e8740858de83444",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "ae29145157b8e7772f06",
+    "url": "/game-of-life/static/css/main.04eb33fa.chunk.css"
+  },
+  {
+    "revision": "4287534af42f6391e983",
+    "url": "/game-of-life/static/js/2.9ec0c91e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.9ec0c91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae29145157b8e7772f06",
+    "url": "/game-of-life/static/js/main.71c4a761.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
